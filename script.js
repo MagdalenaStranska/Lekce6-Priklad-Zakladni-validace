5d306985899d659c4d87e36d512c0a24f6a7ad75
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-10-28 19:28:12 +0100

    Pridavam reseni

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log(validator.isEmail("stranska.magdalena@gmail.com"))
+console.log(validator.isCreditCard("4125010001000208", ""))
+console.log(validator.isCreditCard("5443226010393168", ""))
+
+const email = prompt("Zadej e-mail")
+
+const message = document.querySelector(".msg")
+
+if(validator.isEmail(email)) {
+    message.textContent = "E-mail v pořádku";
+    message.classList.add('msg--valid');
+    } else {
+    message.textContent = "Neplatný e-mail";
+    message.classList.add('msg--invalid');
+    }
