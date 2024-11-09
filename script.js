894a68ad3f4dd3ff57ae33f821dde5317f54fe73
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-11-10 00:38:03 +0100

    Pridavam reseni lektora

--- script.js
+++ script.js
@@ -2,14 +2,27 @@ console.log(validator.isEmail("stranska.magdalena@gmail.com"))
 console.log(validator.isCreditCard("4125010001000208", ""))
 
 
-const email = prompt("Zadej e-mail")
+// const email = prompt("Zadej e-mail")
 
-const message = document.querySelector(".msg")
+// const message = document.querySelector(".msg")
 
-if(validator.isEmail(email)) {
-    message.textContent = "E-mail v pořádku";
-    message.classList.add('msg--valid');
-    } else {
-    message.textContent = "Neplatný e-mail";
-    message.classList.add('msg--invalid');
-    }
+// if(validator.isEmail(email)) {
+//     message.textContent = "E-mail v pořádku";
+//     message.classList.add('msg--valid');
+//     } else {
+//     message.textContent = "Neplatný e-mail";
+//     message.classList.add('msg--invalid');
+//     }
+
+const email = prompt('Zadejte váš e-mail');
+
+const isEmail = validator.isEmail(email);
+const messageElement = document.querySelector('#msg');
+
+if (isEmail) {
+  messageElement.textContent = 'E-mail v pořádku';
+  messageElement.classList.add('msg--valid');
+} else {
+  messageElement.textContent = 'Neplatný e-mail';
+  messageElement.classList.add('msg--invalid');
+}
